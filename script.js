d999d4a3df2ee581c503b624052f4e7814067941
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-04-07 00:21:54 +0200

    Add index and script projects to repository

---
+++ script.js
@@ -0,0 +1,9 @@
+var withMenuItemsClass = document.getElementsByClassName('button');
+
+
+for(i = 0; i < withMenuItemsClass.length; i++) {
+	console.log(withMenuItemsClass[i].innerHTML);
+	alert(withMenuItemsClass[i].innerHTML);
+}
+
+
